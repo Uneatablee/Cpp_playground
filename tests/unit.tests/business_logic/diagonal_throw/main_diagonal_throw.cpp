b6185ea54b505bfba9f210642441d1162d71c422
Jakub <jakubpasek11@gmail.com>
2023-07-19 23:18:31 +0200

    last changes in initial structure

--- tests/unit.tests/business_logic/diagonal_throw/main_diagonal_throw.cpp
+++ tests/unit.tests/business_logic/diagonal_throw/main_diagonal_throw.cpp
@@ -3,7 +3,8 @@
 #include <catch2/catch_test_macros.hpp>
 
 
-TEST_CASE( "Bus_stop library working test" )
+TEST_CASE( "Diagonal_throw library working test" )
 {
     REQUIRE(cpp_playground::pg_business_logic::diagonal_throw::throw_draw(3,5) == 10.0);
 }
+
