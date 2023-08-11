b405053503011088b683927ac4726361f8d000f6
Jakub <jakubpasek11@gmail.com>
2023-08-11 12:26:55 +0200

    Applied changes to interfaces acoording to rule of 5 and Scotts's Meyers effective modern c++

--- src/business_logic/bus_stop/include/IBus_stop.hpp
+++ src/business_logic/bus_stop/include/IBus_stop.hpp
@@ -11,6 +11,13 @@ namespace cpp_playground::pg_business_logic::bus_stop
     public:
 
         virtual ~Ibus_stop() = default;
+
+        Ibus_stop(Ibus_stop&&) = default;
+        Ibus_stop& operator=(Ibus_stop&&) = default;
+
+        Ibus_stop(const Ibus_stop&) = default;
+        Ibus_stop& operator=(const Ibus_stop&) = default;
+
         virtual void drawStop() = 0;
         virtual void refreshStop() = 0;
         virtual int counterBussesStop() = 0;
--- src/business_logic/bus_stop/include/IVehicle.hpp
+++ src/business_logic/bus_stop/include/IVehicle.hpp
@@ -11,6 +11,13 @@ namespace cpp_playground::pg_business_logic::bus_stop
         enum class Movement{Forward, Backward};
 
         virtual ~Ivehicle() = default;
+
+        Ivehicle(Ivehicle&&) = default;
+        Ivehicle& operator=(Ivehicle&&) = default;
+
+        Ivehicle(const Ivehicle&) = default;
+        Ivehicle& operator=(const Ivehicle&) = default;
+
         virtual void moveVehicle(Movement) = 0;
         virtual void drawVehicle() = 0;
         virtual std::string name_checkVehicle() = 0;
