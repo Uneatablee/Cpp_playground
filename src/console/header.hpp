2513d174b6988dab06edbdb96b51855c52125a32
Jakub <jakubpasek11@gmail.com>
2023-09-03 23:11:14 +0200

    added addidtional tests

--- src/console/header.hpp
+++ src/console/header.hpp
@@ -1,8 +1,1 @@
 #pragma once
-
-#include <iostream>
-#include "diagonal_throw_functions.hpp"
-#include "Tbus.hpp"
-#include "Tstop.hpp"
-#include "Tcar.hpp"
-#include "Tterminal.hpp"
--- tests/unit.tests/business_logic/bus_stop/main_bus_stop.cpp
+++ tests/unit.tests/business_logic/bus_stop/main_bus_stop.cpp
@@ -68,6 +68,9 @@ TEST_CASE("When bus moves beyond terminal it changes direction and goes back tow
     std::unique_ptr<Ivehicle> bus_1 = std::make_unique<Tbus>(3, "bus_2");
 
     // Act
+
+    //Forward movement testing------------------------------------------------------
+
     bus -> assignRoute(route_1);
     bus_1 -> assignRoute(route_1);
 
@@ -85,9 +88,34 @@ TEST_CASE("When bus moves beyond terminal it changes direction and goes back tow
     bus_1 -> moveVehicle(Ivehicle::Movement::Forward);
     std::string stop_name_4 = bus_1 -> showStop();
 
+    //Backward movement testing------------------------------------------------------
+
+    std::unique_ptr<Ivehicle> car_1 = std::make_unique<Tcar>(2, "car_1");
+    std::unique_ptr<Ivehicle> car_2 = std::make_unique<Tcar>(2, "car_2");
+
+    car_1 -> assignRoute(route_1);
+    car_2 -> assignRoute(route_1);
+
+    car_1 -> moveVehicle(Ivehicle::Movement::Forward);
+    car_1 -> moveVehicle(Ivehicle::Movement::Forward);
+    std::string car_position_1 = car_1 -> showStop();
+
+    car_2 -> moveVehicle(Ivehicle::Movement::Backward);
+    car_2 -> moveVehicle(Ivehicle::Movement::Backward);
+    std::string car_position_2 = car_2 -> showStop();
+
+
+    for(int i = 0; i < 4; i++)
+    {
+        car_2 -> moveVehicle(Ivehicle::Movement::Backward);
+    }
+    std::string car_position_3 = car_2 -> showStop();
 
     // Assert
 
     REQUIRE(stop_name_1 == stop_name_2);
     REQUIRE(stop_name_3 == stop_name_4);
+    REQUIRE(car_position_1 == car_position_2);
+    REQUIRE(car_position_1 == car_position_3);
+
 }
